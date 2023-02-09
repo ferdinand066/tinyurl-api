adf6a9ff20dc6e19339f2b067a06752715f304e2
Ferdinand Gunawan <ferdinandg066@gmail.com>
2023-02-09 11:38:07 +0700

    feat: check hai

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function(){
+    return 'hai';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
